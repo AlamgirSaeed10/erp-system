a9a8329fe5815ce5822dc59acfa18aaf323d07f7
usama-shakeel001 <103742143+usama77shakeel@users.noreply.github.com>
2022-04-19 12:53:14 +0500

    Update EmployeeController.php

--- app/Http/Controllers/EmployeeController.php
+++ app/Http/Controllers/EmployeeController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Http\Request;
 use DataTables;
-
+use App\Http\Controllers\Controller;
 class EmployeeController extends Controller
 {
     function show_departments()
